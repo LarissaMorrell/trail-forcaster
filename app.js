f20e49bd5785fb7b19ea6fd731823c2d8a6bdeee
LarissaMorrell <larissa@larissamorrell.com>
2016-12-18 20:21:38 -0500

    fixed individual result selection, historic and forecast weather written in. Need to find out more about history weather API and accessing the  forecastday[]

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var BASE_TRAIL_URL = 'https://trailapi-trailapi.p.mashape.com/';
 var BASE_WEATHER_URL = 'http://api.apixu.com/v1/';
 var activityDate;
-var dateTense;
+//make an object for historic and forecasted data?
 
-function getDataFromTrailApi(cityQuery, stateQuery, callback) {
+function getDataFromTrailApi(callback) {
     var data = {
-        url: BASE_TRAIL_URL + getEndpoints(cityQuery, stateQuery), // The URL to the API. You can get this in the API page of the API you intend to consume
+        url: BASE_TRAIL_URL + getEndpoints(), // The URL to the API. You can get this in the API page of the API you intend to consume
         type: 'GET',
         data: {}, // Additional parameters here
         dataType: 'json',
@@ -22,14 +22,18 @@ function getDataFromTrailApi(cityQuery, stateQuery, callback) {
 }
 
 
-function getEndpoints(cityQuery, stateQuery) {
-    var endString = '';
 
-    if (cityQuery.length > 0) {
-        endString += '?q[city_cont]=' + cityQuery +
-            '&q[state_cont]=' + stateQuery;
+
+function getEndpoints() {
+    var endString = '';
+    var city = $('.js-city').val();
+    var state = $('.js-state').val();
+
+    if (city.trim() != "") {
+        endString += '?q[city_cont]=' + city +
+            '&q[state_cont]=' + state;
     } else {
-        endString += '?q[state_cont]=' + stateQuery;
+        endString += '?q[state_cont]=' + state;
     }
 
     return endString;
@@ -37,40 +41,38 @@ function getEndpoints(cityQuery, stateQuery) {
 
 
 
-function getDataFromWeatherApi(locObj, callback) { //also pass in lat/long param from search result
-
-    setDateTense();
+function getDataFromWeatherApi(locObj, dateTense, endpoint, callback) { //also pass in lat/long param from search result
 
     var settings = {
         url: BASE_WEATHER_URL + dateTense +
-            '.json?key=245141566e984e7e9de230727161012&q=' +  locObj.lat+
-            ',' + locObj.lon,
+            '.json?key=245141566e984e7e9de230727161012&q=' + locObj.lat +
+            ',' + locObj.lon + endpoint,
         dataType: 'json',
         data: {},
         type: 'GET',
         success: callback
     };
 
-    console.log(settings.url);
+    console.log(dateTense + ' weather: ' + settings.url);
 
     $.ajax(settings);
 }
 
 
 
-function setDateTense() {
-    //convert the 2 string into a Date???
-    //then compare and 
-    var today = todaysDate;
+// function setDateTense() {
+//     //convert the 2 string into a Date???
+//     //then compare and 
+//     var today = todaysDate;
 
-    if (activityDate < today) {
-        dateTense = 'history';
-    } else if (activityDate == today) {
-        dateTense = 'current';
-    } else {
-        dateTense = 'forecast';
-    };
-}
+//     if (activityDate < today) {
+//         dateTense = 'history';
+//     } else if (activityDate == today) {
+//         dateTense = 'current';
+//     } else {
+//         dateTense = 'forecast';
+//     };
+// }
 
 
 
@@ -86,20 +88,20 @@ function formatDate(date) {
     return [year, month, day].join('-');
 }
 
-function todaysDate() {
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
+// function todaysDate() {
+//     var today = new Date();
+//     var dd = today.getDate();
+//     var mm = today.getMonth() + 1; //January is 0!
 
-    var yyyy = today.getFullYear();
-    if (dd < 10) {
-        dd = '0' + dd
-    }
-    if (mm < 10) {
-        mm = '0' + mm
-    }
-    return yyyy + '-' + mm + '-' + dd;
-}
+//     var yyyy = today.getFullYear();
+//     if (dd < 10) {
+//         dd = '0' + dd
+//     }
+//     if (mm < 10) {
+//         mm = '0' + mm
+//     }
+//     return yyyy + '-' + mm + '-' + dd;
+// }
 
 
 // Render functions
@@ -108,25 +110,73 @@ function displaySearchData(data) {
     if (data.places.length > 0) {
         data.places.forEach(function(place) {
 
-            $('.js-search-results').append('<div class="result small"' + 
-                'data-lat="' + place.lat + '" data-lon="' + place.lon + '">' +
+            $('.js-search-results').append('<div class="result small"' +
+                'data-lat="' + place.lat + '" data-lon="' + place.lon + '" ' +
+                'data-city="' + place.city + '" data-state="' + place.state + '">' +
                 '<div class="result-name">' + place.name + '</div>' +
                 '<div class="result-location">' + place.city + ', ' + place.state + '</div></div>');
         });
+
     } else {
         $('.js-search-results').append('<p>Sorry, no trails found</p>');
     }
 
 }
 
-function displayTrailData(data){
 
+
+// but how do I target what activity it is that was selected when API is called again??????
+function displayTrailData(data) {
+    var trailObj = data.places.activities;
+
+    //for every activity in the city    --- no there are multiple city objects....
+    // for (var x = 0; x < trailObj.length; x++) {
+
+    //if the place id matches, then append ---> store place id in data???
+
+    var trailSection = '<div class="trail-details">Description: ' +
+        trailObj.description + '</p>' +
+        '<p>See more about this trail: <a href="' + trailObj.url + '>Click here!</a></p>' +
+        '</div>';
+    // }
+
+    return trailSection;
 }
 
 
-function displayWeatherData(SearchLocation) {
+//display the previous 3 days of weather
+function displayHistoricWeatherData(data) {
+    var forecastDays = data.forecast.forecastday;
 
+    for (var i = 0; i < forecastDays.length; i++) {
+        $('.big').append('<p>Rainfall: ' + forecastDays[i].day.totalprecip_in + ' inches</p>');
+    }
+}
 
+//display the current day and next 3 days of weather
+function displayWeatherData(data) {
+    var forecastDays = data.forecast.forecastday;
+
+    //display the last 3 days rainfall/snow/temp
+    for (var i = 0; i < forecastDays.length; i++) {
+        $('.big').append("<p>Working... " + forecastDays[i].day.maxtemp_f + "degrees</p>");
+    }
+    //display the next 4 days temp/rainfall/snow/wind
+
+}
+
+function expandResult(locObj) {
+    $('.big').append('<section>' +
+        // getDataFromTrailApi(locObj.city, locObj.state, displayTrailData) +
+        '<div class="weather-details"></div></section>');
+
+    getDataFromTrailApi($(this).data('city'), $(this).data('state'), displayTrailData);
+
+    //need help getting the date 3 days prior to the current date!!!!
+    getDataFromWeatherApi(locObj.data(), 'history', '&days=3&dt=2016-12-15', displayHistoricWeatherData);
+    // getDataFromWeatherApi(locObj.data(), 'history', '&dt=2016-12-16', displayHistoricWeatherData);
+    // getDataFromWeatherApi(locObj.data(), 'history', '&dt=2016-12-17', displayHistoricWeatherData);
+    getDataFromWeatherApi(locObj.data(), 'forecast', '&days=3', displayWeatherData);
 }
 
 
@@ -140,14 +190,7 @@ function watchSubmit() {
 
         activityDate = $('.js-date').val();
 
-        var stateQuery = $(this).find('.js-state').val();
-        var cityQuery = $(this).find('.js-city').val();
-
-        // var SearchLocation = 
-        getDataFromTrailApi(cityQuery, stateQuery, displaySearchData);
-        // console.log(typeof SearchLocation);
-        // getDataFromWeatherApi(SearchLocation);
-        // getDataFromWeatherApi();
+        getDataFromTrailApi(displaySearchData);
     });
 }
 
@@ -156,19 +199,27 @@ function watchSubmit() {
 $(document).ready(function() {
     var now = new Date();
     var date = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
-
+    console.log(typeof date + ' is the date');
     //set the value of element to today's date on load
     $('.js-date').val(date);
 
+
     watchSubmit();
 
+    //Expand the result
     $(document).on('click', '.small', function(event) {
         $(this).addClass('big');
         $(this).removeClass('small');
-        alert("lon: " + $(this).data('lon') + "lat: " + $(this).data('lat'));
-        getDataFromWeatherApi($(this).data(), displayWeatherData);
+
+        expandResult($(this));
     });
+
+
+
+    //Collapse the result
     $(document).on('click', '.big', function() {
+        $('.result-location').nextAll().remove();
+
         $(this).addClass('small');
         $(this).removeClass('big');
     });
